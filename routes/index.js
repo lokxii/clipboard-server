0541e326758fe277abffdcc1733f0160306d0bda
lokxii <9tin9tin9@gmail.com>
2026-02-23 11:50:48 +0800

    Skip prefetch when copy

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,3 @@
-let mime = "";
-
 /**
  * @param {string} htmlStr
  */
@@ -13,17 +11,17 @@ function create_element(htmlStr) {
     return frag;
 }
 
-function preflight_fragment() {
-    return create_element(
-        '<button id="preflight" type="button" onclick="preflight()">' +
-            'Preflight' +
-        '</button>');
-}
-
 function paste_fragment() {
     return create_element(
         '<button id="paste" type="button" onclick="paste()">' +
             'Paste' +
+        '</button>');
+}
+
+function copy_fragment() {
+    return create_element(
+        '<button id="copy" type="button" onclick="preflight()">' +
+            'Copy' +
         '</button>');
 }
 
@@ -45,11 +43,6 @@ function reset_fragment() {
 }
 
 function preflight() {
-    // const image_elem = document.getElementById("image");
-    // if (image_elem !== null) {
-    //     image_elem.remove();
-    // }
-
     const uri = window.location + "api/v1/copy-from-clipboard";
     setTimeout(async () => {
         const res = await fetch(uri, { method: "options" });
@@ -57,43 +50,28 @@ function preflight() {
             window.alert("Nothing to copy?");
         }
 
-        mime = res.headers.get("Content-Type");
+        const mime = res.headers.get("Content-Type");
         if (mime.startsWith("text/")) {
-            const fragment = create_element(
-                '<button id="copy" type="button" onclick="copy()">' +
-                    'Copy' +
-                '</button>');
-            document.getElementById("preflight").replaceWith(fragment);
+            setTimeout(async () => {
+                await navigator.clipboard.write([
+                    new ClipboardItem({
+                        [mime]: Promise.resolve(await res.text())
+                    })
+                ]);
+            });
             return;
         }
         if (mime.startsWith("image/")) {
-            document.getElementById("preflight").replaceWith(image_fragment(uri));
+            document.getElementById("copy").replaceWith(image_fragment(uri));
             document.getElementById("paste").replaceWith(reset_fragment());
             return;
         }
     });
 }
 
-function copy() {
-    const uri = window.location + "api/v1/copy-from-clipboard";
-    setTimeout(async () => {
-        const blob_promise = async () => {
-            const res = await fetch(uri);
-            return await res.blob()
-        };
-        await navigator.clipboard.write([
-            new ClipboardItem({
-                [mime]: blob_promise()
-            })
-        ])
-
-        document.getElementById("copy").replaceWith(preflight_fragment());
-    });
-}
-
 function reset() {
     document.getElementById("reset").replaceWith(paste_fragment());
-    document.getElementById("image").replaceWith(preflight_fragment());
+    document.getElementById("image").replaceWith(copy_fragment());
 }
 
 function paste() {
